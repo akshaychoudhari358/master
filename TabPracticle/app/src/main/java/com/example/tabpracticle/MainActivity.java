148330e9d0f34bf6e1d346636fa891d2df1e8913
akshaychoudhari358 <akshaychoudhari358@gmail.com>
2019-09-14 18:30:21 +0530

    initial commit of tab practicle

---
+++ TabPracticle/app/src/main/java/com/example/tabpracticle/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.tabpracticle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+
+        final MyAdapter myAdapter = new MyAdapter(this, getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(myAdapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+}
